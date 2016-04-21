fe6466a4b957a38caf74edd8fce30930deed9af5
Brandon Johnston <brandonjohnston@web.de>
2016-04-21 18:04:32 +0200

    Klasse Controller intern in Program.cs initialisieren

--- Awsome-Bingo-Karten-Generator/Controller.cs
+++ Awsome-Bingo-Karten-Generator/Controller.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Awsome_Bingo_Karten_Generator
+﻿namespace Awsome_Bingo_Karten_Generator
 {
-    class Controller
+    internal class Controller
     {
     }
 }
