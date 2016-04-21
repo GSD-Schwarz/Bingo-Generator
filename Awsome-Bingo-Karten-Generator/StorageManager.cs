956bd3717989aa4b3370a4000449a76105dd967f
Sebastian Schlegel <SebastianSchlegel1@gmx.net>
2016-04-21 14:51:50 +0200

    Class Bingoformular  StorageManager

---
+++ Awsome-Bingo-Karten-Generator/StorageManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pdf.;
+
+namespace Awsome_Bingo_Karten_Generator
+{
+    class StorageManager
+    {
+        private 
+        public void lesenBilder()
+        {
+            foreach (var bilderkarte in bilderkarten)
+            {
+
+            }
+        }
+
+    }
+}
