aff6ed2fc3735d52760c9abe0b4360051bd44357
brandonj35578 <brandonjohnston@web.de>
2016-06-08 12:09:58 +0200

    Adding controller

---
+++ Awsome-Bingo-Karten-Generator/Controller.cs
@@ -0,0 +1,11 @@
+﻿namespace Awsome_Bingo_Karten_Generator
+{
+    class Controller
+    {
+        public Controller()
+        {
+            Bingoformular _Bingoformular = new Bingoformular();
+            StorageManager _StorageManager = new StorageManager();
+        }
+    }
+}
